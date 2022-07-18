0d5db3663d68a52e84c4330fd2ec87b39ce93040
unknown <mishravedant040@gmail.com>
2022-07-18 16:51:30 +0530

    control-statement-assignment-basic

---
+++ Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Linq;
+
+namespace Control_Statement_Assignment_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                                                                            * if-else statement
+             
+            int age;
+            Console.WriteLine("Hi Please Enter The Your Age:");
+            age = Convert.ToInt32(Console.ReadLine());
+            if(age > 18)
+            {
+                Console.WriteLine("You are eligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible");
+            }
+            */
+
+            /*                                                               nested if statement
+                                                             
+            string first_name;
+            int roll_no;
+            Console.WriteLine("Please enter you roll no:(1 to 10):");
+            roll_no = Convert.ToInt32(Console.ReadLine());
+            if(roll_no < 10)
+            {
+                Console.WriteLine("Now Please enter your name (Type: vedant)");
+                first_name = Console.ReadLine();
+                if(first_name == "vedant")
+                {
+                    Console.WriteLine("Now you can see your details");
+                }
+                else
+                {
+                    Console.WriteLine("user name is not matched");
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter the valid rool no");
+            }
+            */
+
+            /*
+                                                                            if-else-if ladder
+             
+            int marks;
+            int roll_no;
+            Console.WriteLine("Please enter the student roll no (1 to 10):");
+            roll_no = Convert.ToInt32(Console.ReadLine());
+            if(roll_no < 10)
+            {
+                Console.WriteLine("Please enter the student marks for displaying grades:");
+                marks = Convert.ToInt32(Console.ReadLine());
+                if(marks >= 90 && marks <= 100)
+                {
+                    Console.WriteLine("Congrats you got A+ Grade");
+                }
+                else if(marks >= 80 && marks < 90)
+                {
+                    Console.WriteLine("Congrats you got A Grade");
+                }
+                else if(marks >= 70 && marks < 80)
+                {
+                    Console.WriteLine("Congrats you got B+ Grade");
+                }
+                else if(marks >= 65 && marks < 70)
+                {
+                    Console.WriteLine("Congrats you got B Grade");
+                }
+                else if(marks >= 50 && marks < 65)
+                {
+                    Console.WriteLine("Congrats you got C+ Grade");
+                }
+                else if(marks >= 35 && marks < 50)
+                {
+                    Console.WriteLine("Congrats you got C Grade");
+                }
+                else if(marks < 35)
+                {
+                    Console.WriteLine("Do work hard and change the mindset of others on you");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the valid Marks");
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter the valid rool no:");
+            }
+            */
+
+            /*
+             *                                                                Switch case 
+             
+            string color;
+            Console.WriteLine("Enter the first letter of color (g,b,r,w):");
+            color = Console.ReadLine();
+            switch(color)
+            {
+                case "g":
+                    Console.WriteLine("You are plants lover");
+                    break;
+                case "b":
+                    Console.WriteLine("You are sky lover");
+                    break;
+                case "r":
+                    Console.WriteLine("Your are so aggressive");
+                    break;
+                case "w":
+                    Console.WriteLine("You are peace lover");
+                    break;
+                default:
+                    Console.WriteLine("You enter the invalid color :");
+                    break;
+            }
+            */
+
+            /*
+             *                                                               For Loop 
+             
+            int number;
+            int fact = 1;
+            Console.WriteLine("Which number you want to know factorial:");
+            number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial number of "+ number +" is " + fact);
+            */
+
+            /*
+             *                                                                 While Loop
+             
+            int number;
+            int fact = 1;
+            int i = 1;
+            Console.WriteLine("Which number you want to know factorial:");
+            number = Convert.ToInt32(Console.ReadLine());
+            while(i <= number)
+            {
+                fact = fact * i;
+                i++;
+            }
+            Console.WriteLine("Factorial number of " + number + " is " + fact);
+            */
+
+            /*
+             *                                                               Do While Loop
+             
+            int number;
+            int fact = 1;
+            int i = 1;
+            Console.WriteLine("Which number you want to know factorial:");
+            number = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                i++;
+                Console.WriteLine("Factorial number of " + number + " is " + fact);
+                fact = fact * i;
+                
+            } while (i <= number);
+            */
+
+
+            /*
+             *                                                              ForEach Loop 
+             
+            int number;
+            int fact = 1;
+            Console.WriteLine("Which number you want to know factorial:");
+            number = Convert.ToInt32(Console.ReadLine());
+            foreach(int i in Enumerable.Range(1,number))
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial number of " + number + " is " + fact);
+            */
+
+        //    float a;
+        //    a = float.Parse(Console.ReadLine());
+         //   Console.WriteLine("type cast: " + a);  // for checking type casting
+           
+
+        }
+    }
+}
